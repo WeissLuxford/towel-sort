acb41bfbd0c65bafacc8a6c8ab18a08ed4dd662e
Weiss Luxford <weissluxford@gmail.com>
2022-07-17 21:34:04 +0500

    solvedTask

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (...matrix) {
+  const lo = [] 
+  const valuePusher = (prop) => {
+    for (const key of prop) {
+      lo.push(key)
+    }
+  }
+  matrix.forEach((key) => key.forEach((value, index) => {
+    if ( index % 2 !== 0) {
+      value.reverse()
+      valuePusher(value)
+    } else{
+      valuePusher(value)
+    }
+}) )
+return lo
 }
